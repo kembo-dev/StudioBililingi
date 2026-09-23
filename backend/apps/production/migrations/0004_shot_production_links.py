c80dfcb824b02c5d95b7dc7d699e35cdc8b1216c
alexandre00007 <58002843+alexandre00007@users.noreply.github.com>
2026-09-23 12:56:52 +0100

    feat: migrate shot production links

---
+++ backend/apps/production/migrations/0004_shot_production_links.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("story", "0007_shot_shottake"),
+        ("production", "0003_asset_beat_take_review_beat_take"),
+    ]
+
+    operations = [
+        migrations.AddField(model_name="asset", name="shot", field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name="assets", to="story.shot")),
+        migrations.AddField(model_name="asset", name="shot_take", field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name="assets", to="story.shottake")),
+        migrations.AddField(model_name="review", name="shot", field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name="reviews", to="story.shot")),
+        migrations.AddField(model_name="review", name="shot_take", field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name="reviews", to="story.shottake")),
+    ]
